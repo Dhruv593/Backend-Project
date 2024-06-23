d92b13dadba6d84841d8779257ad71d816ee2d4d
Dhruv Lad <dhruvlad4444@gmail.com>
2024-06-23 17:56:31 +0530

    Add register controller

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import { DB_NAME } from './constants.js';
 import connectDB from "./db/index.js";
-import express from 'express';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env',
 });
-
-const app = express()
 
 connectDB()
 .then(() => {
